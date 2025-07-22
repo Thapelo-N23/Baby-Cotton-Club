c0a4e80a6ef310c1cccbca378f8e570013a66921
Olwethu-Nene <230277845@mycput.ac.za>
2025-07-22 05:20:29 +0200

    Updated classes

---
+++ src/test/java/za/ac/cput/controller/CategoryControllerTest.java
@@ -0,0 +1,20 @@
+package za.ac.cput.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryControllerTest {
+
+    @Test
+    void createCategory() {
+    }
+
+    @Test
+    void readCategory() {
+    }
+
+    @Test
+    void getAllCategories() {
+    }
+}
---
+++ src/test/java/za/ac/cput/controller/ReviewControllerTest.java
@@ -0,0 +1,24 @@
+package za.ac.cput.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReviewControllerTest {
+
+    @Test
+    void createReview() {
+    }
+
+    @Test
+    void readReview() {
+    }
+
+    @Test
+    void updateReview() {
+    }
+
+    @Test
+    void getAllReviews() {
+    }
+}
