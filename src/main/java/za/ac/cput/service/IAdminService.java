49d59ccc3bc8e8b72666ca36f02184faa7fc7f6c
tsireledzonetshilonwe <165159482+tsireledzonetshilonwe@users.noreply.github.com>
2025-09-02 12:37:40 +0200

    update

---
+++ src/main/java/za/ac/cput/service/IAdminService.java
@@ -0,0 +1,11 @@
+package za.ac.cput.service;
+
+import za.ac.cput.domain.Admin;
+import za.ac.cput.domain.CustomerOrder;
+
+import java.util.List;
+
+public interface IAdminService extends IService<Admin, Integer> {
+    void delete(int adminId);
+    List<Admin> getAll();
+}
