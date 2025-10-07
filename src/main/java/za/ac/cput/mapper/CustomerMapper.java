ea689b727e71c28f75fdd8e2ace4aa80abee9b69
Mengezi Junior Ngwenya <162158030+mengezingwenya1@users.noreply.github.com>
2025-10-07 18:58:21 +0200

    Update

---
+++ src/main/java/za/ac/cput/mapper/CustomerMapper.java
@@ -0,0 +1,26 @@
+package za.ac.cput.mapper;
+
+import za.ac.cput.domain.Customer;
+import za.ac.cput.dto.CustomerResponse;
+
+import java.util.stream.Collectors;
+
+public class CustomerMapper {
+    public static CustomerResponse toDto(Customer customer) {
+        CustomerResponse dto = new CustomerResponse();
+        dto.setCustomerId(customer.getCustomerId());
+        dto.setFirstName(customer.getFirstName());
+        dto.setLastName(customer.getLastName());
+        dto.setEmail(customer.getEmail());
+
+        if (customer.getOrders() != null) {
+            dto.setOrders(
+                    customer.getOrders().stream()
+                            .map(CustomerOrderMapper::toDto)
+                            .collect(Collectors.toList())
+            );
+        }
+
+        return dto;
+    }
+}
