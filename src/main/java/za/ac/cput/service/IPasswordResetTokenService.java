fae1cad08b8cf21cab1e3185d819ef2291931f59
Mengezi Junior Ngwenya <162158030+mengezingwenya1@users.noreply.github.com>
2025-10-19 21:43:08 +0200

    Update

--- src/main/java/za/ac/cput/service/IPasswordResetTokenService.java
+++
@@ -1,8 +0,0 @@
-package za.ac.cput.service;
-
-import za.ac.cput.domain.PasswordResetToken;
-
-public interface IPasswordResetTokenService extends IService <PasswordResetToken, Long>{
-    PasswordResetToken findByToken(String token);
-
-}
